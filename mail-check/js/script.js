b41fb4ef808b4f6c899ef1ae4b7386c426c32d07
federico_viganò <fede_viga@hotmail.it>
2024-02-01 16:47:30 +0100

    completata struttura js mail

--- mail-check/js/script.js
+++ mail-check/js/script.js
@@ -5,3 +5,31 @@ controlla che sia nella lista di chi può accedere (qui quindi dovremmo creare d
 stampa un messaggio appropriato sull’esito del controllo.
 (NON utilizzate il metodo .includes())
 */
+
+
+// inizializzo array con le email
+const emails =  ["albusdumbledore@hogwarts.wiz", "nevillelongbottom@hogwarts.wiz", "hermionegranger@hogwarts.wiz", "harrypotter@hogwarts.wiz", "ronaldweasley@hogwarts.wiz", "ginnyweasley@hogwarts.wiz", "minervamcgonagall@hogwarts.wiz"];
+console.log(emails)
+
+// inizializzo inputElement
+const inputElement = document.querySelector("#input");
+console.log(inputElement);
+
+// inizializzo buttonElement
+const buttonElement = document.querySelector("#my_button");
+console.log(buttonElement);
+
+// creo variabile di controllo
+let login = false;
+
+// scrivo funzione che al click del bottone controlla se la mail inserita dall'utente è presente nell'array
+buttonElement.addEventListener('click',
+    function() {
+        for (let i = 0; i < emails.length; i++ ) {
+            if (inputElement.value == emails[i]) {
+                login = true;
+            }
+        }
+        console.log(login);
+    }
+)
